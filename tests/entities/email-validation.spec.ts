43c192c95d42d8bfc8dded0561253cce9ff402a0
BernardoBF4 <bernardo.fantin@gmail.com>
2023-10-16 21:44:39 -0300

    🧪 test(api: emails): não aceita e-mails com mais de 64 caracteres antes do "@"

--- tests/entities/email-validation.spec.ts
+++ tests/entities/email-validation.spec.ts
@@ -9,11 +9,28 @@ describe('E-mail validation', () => {
     expect(ret).toBeFalsy()
   })
 
+  test('should not accept empty srings', () => {
+    const email: string = ''
+
+    const ret: boolean = Email.validate(email)
+
+    expect(ret).toBeFalsy()
+  })
+
   test('should accept valid e-mail', () => {
     const email: string = 'any@email.com'
 
     const ret: boolean = Email.validate(email)
 
     expect(ret).toBeTruthy()
+  })
+
+  test('should not accept local part larger than 64 characters', () => {
+    const email: string =
+      'askdaskdaskdoaskdoaskdoaskdlsdalçsdalsdasldasldsalçdldaslçasdsads@email.com'
+
+    const ret: boolean = Email.validate(email)
+
+    expect(ret).toBeFalsy()
   })
 })
