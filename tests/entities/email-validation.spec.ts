f689a033e947c4808fa3b8c6d12fa66941d55590
BernardoBF4 <bernardo.fantin@gmail.com>
2023-10-21 16:57:49 -0300

    🧪 test(proj: emails): email não pode conter caracteres inválidos

--- tests/entities/email-validation.spec.ts
+++ tests/entities/email-validation.spec.ts
@@ -73,4 +73,12 @@ describe('E-mail validation', () => {
 
     expect(ret).toBeFalsy()
   })
+
+  test('should not accept local part with invalid characters', () => {
+    const email: string = 'any email@email.com'
+
+    const ret: boolean = Email.validate(email)
+
+    expect(ret).toBeFalsy()
+  })
 })
