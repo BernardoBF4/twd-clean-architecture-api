78f3fa3258f06052fb07f6ffe19e8ae021231c32
BernardoBF4 <bernardo.fantin@gmail.com>
2023-10-24 22:36:54 -0300

    ✨ feat(proj: usuário): validação de nome válido adicionada

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,12 +1,18 @@
 import { Either, left } from '../shared/either'
 import Email from './email'
 import InvalidEmailError from './errors/invalid-email-error'
+import InvalidNameError from './errors/invalid-name-error'
+import Name from './name'
 import UserData from './user-data'
 
 export default class User {
-  static create(user_data: UserData): Either<InvalidEmailError, User> {
-    const email_or_error = Email.create(user_data.email)
+  static create(user_data: UserData): Either<InvalidNameError | InvalidEmailError, User> {
+    const name_or_error = Name.create(user_data.name)
+    if (name_or_error.isLeft()) {
+      return left(new InvalidNameError())
+    }
 
+    const email_or_error = Email.create(user_data.email)
     if (email_or_error.isLeft()) {
       return left(new InvalidEmailError())
     }
